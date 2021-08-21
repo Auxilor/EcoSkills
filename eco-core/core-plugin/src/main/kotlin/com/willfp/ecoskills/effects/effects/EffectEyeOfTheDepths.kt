6527e0eb7c7591de5a52758d51ed343f468d138f
Auxilor <william.favierparsons1@gmail.com>
2021-08-21 13:40:52 +0100

    Implemented eye of the depths

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/effects/effects/EffectEyeOfTheDepths.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/effects/effects/EffectEyeOfTheDepths.kt
@@ -1,8 +1,43 @@
 package com.willfp.ecoskills.effects.effects
 
+import com.willfp.eco.core.drops.DropQueue
+import com.willfp.eco.core.items.Items
+import com.willfp.eco.util.NumberUtils
 import com.willfp.ecoskills.effects.Effect
+import com.willfp.ecoskills.getEffectLevel
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.player.PlayerFishEvent
+import org.bukkit.event.player.PlayerItemDamageEvent
 
 class EffectEyeOfTheDepths: Effect(
     "eye_of_the_depths"
 ) {
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    fun handle(event: PlayerFishEvent) {
+        val player = event.player
+
+        if (event.state != PlayerFishEvent.State.CAUGHT_FISH) {
+            return
+        }
+
+        val level = player.getEffectLevel(this)
+
+        val chance = config.getDouble("chance-per-level") * level
+
+        if (NumberUtils.randFloat(0.0, 100.0) >= chance) {
+            return
+        }
+
+        val items = config.getStrings("rare-loot-items", false)
+
+        val itemName = items[NumberUtils.randInt(0, items.size - 1)]
+
+        val item = Items.lookup(itemName).item
+
+        DropQueue(player)
+            .setLocation(event.caught!!.location)
+            .addItem(item)
+            .push()
+    }
 }
