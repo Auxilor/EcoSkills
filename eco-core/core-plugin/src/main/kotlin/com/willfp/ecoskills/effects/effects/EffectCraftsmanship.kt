202aa01b6a7131e792201873a777f0781d772dc6
Auxilor <william.favierparsons1@gmail.com>
2021-08-21 11:43:02 +0100

    Added craftsmanship effect

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/effects/effects/EffectCraftsmanship.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/effects/effects/EffectCraftsmanship.kt
@@ -1,8 +1,30 @@
 package com.willfp.ecoskills.effects.effects
 
+import com.willfp.eco.util.NumberUtils
 import com.willfp.ecoskills.effects.Effect
+import com.willfp.ecoskills.getEffectLevel
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.player.PlayerItemDamageEvent
 
-class EffectCraftsmanship: Effect(
+class EffectCraftsmanship : Effect(
     "craftsmanship"
 ) {
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    fun handle(event: PlayerItemDamageEvent) {
+        val player = event.player
+        val item = event.item
+
+        if (!player.inventory.itemInMainHand.type.toString().lowercase().contains("axe")) {
+            return
+        }
+
+        val level = player.getEffectLevel(this)
+
+        if (NumberUtils.randFloat(0.0, 100.0) < config.getDouble("percent-less-per-level") * level) {
+            event.isCancelled = true
+        }
+    }
 }
