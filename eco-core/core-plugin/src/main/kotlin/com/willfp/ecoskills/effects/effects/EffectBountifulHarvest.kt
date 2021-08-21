27d6ae8c9a8cb7da2ce5e3f1ca5313a2be9f5beb
Auxilor <william.favierparsons1@gmail.com>
2021-08-21 09:30:33 +0100

    Implemented bountiful harvest and versatile tools

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/effects/effects/EffectBountifulHarvest.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/effects/effects/EffectBountifulHarvest.kt
@@ -1,8 +1,61 @@
 package com.willfp.ecoskills.effects.effects
 
+import com.willfp.eco.core.drops.DropQueue
+import com.willfp.eco.util.NumberUtils
 import com.willfp.ecoskills.effects.Effect
+import com.willfp.ecoskills.getEffectLevel
+import com.willfp.ecoskills.getStatLevel
+import org.bukkit.Material
+import org.bukkit.block.data.Ageable
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockDropItemEvent
 
 class EffectBountifulHarvest: Effect(
     "bountiful_harvest"
 ) {
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    fun handle(event: BlockDropItemEvent) {
+        val block = event.block
+        val player = event.player
+
+        val data = block.blockData
+
+        if (data !is Ageable) {
+            return
+        }
+
+        if (block.type == Material.SUGAR_CANE || block.type == Material.SWEET_BERRY_BUSH || block.type == Material.CACTUS) {
+            return
+        }
+
+        if (data.age != data.maximumAge) {
+            return
+        }
+
+        if (event.items.isEmpty()) {
+            return
+        }
+
+        val level = player.getEffectLevel(this)
+
+        val chance = config.getDouble("chance-per-level") * level
+
+        val base = (chance / 100).toInt()
+
+        if (base >= 1) {
+            for (i in 1..base) {
+                DropQueue(player)
+                    .addItems(*event.items.map { item -> item.itemStack })
+                    .push()
+            }
+        }
+
+        if (NumberUtils.randFloat(0.0, 100.0) < chance - (base * 100)) {
+            DropQueue(player)
+                .addItems(*event.items.map { item -> item.itemStack })
+                .push()
+        }
+    }
 }
--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/effects/effects/EffectVersatileTools.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/effects/effects/EffectVersatileTools.kt
@@ -1,8 +1,39 @@
 package com.willfp.ecoskills.effects.effects
 
+import com.willfp.eco.core.drops.DropQueue
+import com.willfp.eco.util.NumberUtils
 import com.willfp.ecoskills.effects.Effect
+import com.willfp.ecoskills.getEffectLevel
+import org.bukkit.Material
+import org.bukkit.block.data.Ageable
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.block.BlockDropItemEvent
+import org.bukkit.event.entity.EntityDamageByEntityEvent
 
 class EffectVersatileTools: Effect(
     "versatile_tools"
 ) {
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    fun handle(event: EntityDamageByEntityEvent) {
+        val player = event.damager
+
+        if (player !is Player) {
+            return
+        }
+
+        if (!player.inventory.itemInMainHand.type.toString().lowercase().contains("pickaxe")) {
+            return
+        }
+
+        val level = player.getEffectLevel(this)
+
+        var multiplier = config.getDouble("percent-more-per-level") * level
+
+        multiplier /= 100
+        multiplier += 1
+
+        event.damage = event.damage * multiplier
+    }
 }
