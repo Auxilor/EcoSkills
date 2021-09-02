275173b0c2c09ceb1beab00e42c1e97659e81e56
Auxilor <william.favierparsons1@gmail.com>
2021-09-02 17:28:34 +0100

    Fixed bad skill multiplier bug

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/EcoSkillsPlayer.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/EcoSkillsPlayer.kt
@@ -48,7 +48,7 @@ private fun Player.cacheSkillExperienceMultiplier(): Double {
     for (permissionAttachmentInfo in this.effectivePermissions) {
         val permission = permissionAttachmentInfo.permission
         if (permission.startsWith(prefix)) {
-            return (permission.substring(permission.lastIndexOf(".") + 1).toDoubleOrNull() ?: 100.0 / 100) + 1
+            return ((permission.substring(permission.lastIndexOf(".") + 1).toDoubleOrNull() ?: 100.0) / 100) + 1
         }
     }
 
