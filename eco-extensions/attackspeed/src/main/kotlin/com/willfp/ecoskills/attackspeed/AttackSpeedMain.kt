2d42baf5b09cb8325f92e45c4a04acaffbb22433
Auxilor <william.favierparsons1@gmail.com>
2021-10-05 19:04:51 +0100

    Added attack speed extension

---
+++ eco-extensions/attackspeed/src/main/kotlin/com/willfp/ecoskills/attackspeed/AttackSpeedMain.kt
@@ -0,0 +1,14 @@
+package com.willfp.ecoskills.attackspeed
+
+import com.willfp.eco.core.EcoPlugin
+import com.willfp.eco.core.extensions.Extension
+
+class AttackSpeedMain(plugin: EcoPlugin) : Extension(plugin) {
+    override fun onEnable() {
+        // Do nothing
+    }
+
+    override fun onDisable() {
+        // Do nothing
+    }
+}
