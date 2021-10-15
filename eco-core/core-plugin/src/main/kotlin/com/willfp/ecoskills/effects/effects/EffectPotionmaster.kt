aa02c00db89db5407460e3a72ea5bfc5d23623d9
Auxilor <william.favierparsons1@gmail.com>
2021-10-15 10:04:22 +0100

    Fixed Potionmaster NPE

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/effects/effects/EffectPotionmaster.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/effects/effects/EffectPotionmaster.kt
@@ -136,7 +136,7 @@ class EffectPotionmaster : Effect(
             effects[PotionEffectType.SLOW] = 4
             effects[PotionEffectType.DAMAGE_RESISTANCE] = 2
         } else {
-            effects[data.type.effectType!!] = if (data.type.isUpgradeable) 2 else 1
+            effects[data.type.effectType ?: return] = if (data.type.isUpgradeable) 2 else 1
         }
 
         for (entity in entities) {
