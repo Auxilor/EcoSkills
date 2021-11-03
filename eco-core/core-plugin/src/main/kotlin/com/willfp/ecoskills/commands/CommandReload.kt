f98534b5e1b253be4fc088a46e9bdca51ee19cb7
Auxilor <william.favierparsons1@gmail.com>
2021-11-03 19:06:05 +0000

    Fixed CommandReload.kt

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/commands/CommandReload.kt
@@ -3,7 +3,6 @@ package com.willfp.ecoskills.commands
 import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.command.CommandHandler
 import com.willfp.eco.core.command.impl.Subcommand
-import com.willfp.ecoskills.data.storage.PlayerProfile
 import org.bukkit.command.CommandSender
 
 class CommandReload(plugin: EcoPlugin) :
@@ -15,7 +14,6 @@ class CommandReload(plugin: EcoPlugin) :
     ) {
     override fun getHandler(): CommandHandler {
         return CommandHandler { sender: CommandSender, _: List<String> ->
-            PlayerProfile.saveAll(false)
             plugin.reload()
             sender.sendMessage(plugin.langYml.getMessage("reloaded"))
         }
