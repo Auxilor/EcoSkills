d74fb152e5e7057462143c3ee724444747b8ae79
Auxilor <william.favierparsons1@gmail.com>
2021-12-14 19:20:29 +0000

    Updated attack speed

--- eco-extensions/attackspeed/src/main/java/com/willfp/ecoskills/attackspeed/AttackSpeedConfig.java
+++ eco-extensions/attackspeed/src/main/java/com/willfp/ecoskills/attackspeed/AttackSpeedConfig.java
@@ -1,11 +1,12 @@
 package com.willfp.ecoskills.attackspeed;
 
 import com.willfp.eco.core.PluginLike;
-import com.willfp.eco.core.config.yaml.YamlBaseConfig;
+import com.willfp.eco.core.config.BaseConfig;
+import com.willfp.eco.core.config.ConfigType;
 import org.jetbrains.annotations.NotNull;
 
-public class AttackSpeedConfig extends YamlBaseConfig {
+public class AttackSpeedConfig extends BaseConfig {
     public AttackSpeedConfig(@NotNull final PluginLike plugin) {
-        super("attackspeed", true, plugin);
+        super("attackspeed", plugin, true, ConfigType.YAML);
     }
 }
