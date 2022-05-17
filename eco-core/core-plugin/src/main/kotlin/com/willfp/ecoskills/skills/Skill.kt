defb8ab695fb5cf103eef468beff1e9283d0d58e
Auxilor <william.favierparsons1@gmail.com>
2022-05-17 11:01:47 +0100

    Fixed level xp requirements not updating on /ecoskills reload

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/skills/Skill.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/skills/Skill.kt
@@ -10,10 +10,15 @@ import com.willfp.eco.core.placeholder.PlayerlessPlaceholder
 import com.willfp.eco.util.NumberUtils
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.containsIgnoreCase
-import com.willfp.ecoskills.*
+import com.willfp.ecoskills.EcoSkillsPlugin
+import com.willfp.ecoskills.SkillObject
 import com.willfp.ecoskills.config.SkillConfig
 import com.willfp.ecoskills.effects.Effect
 import com.willfp.ecoskills.effects.Effects
+import com.willfp.ecoskills.getAverageSkillLevel
+import com.willfp.ecoskills.getSkillLevel
+import com.willfp.ecoskills.getSkillProgress
+import com.willfp.ecoskills.getTotalSkillLevel
 import com.willfp.ecoskills.stats.Stats
 import org.bukkit.Bukkit
 import org.bukkit.GameMode
@@ -80,7 +85,12 @@ abstract class Skill(
     fun update() {
         name = config.getFormattedString("name")
         description = config.getFormattedString("description")
+
+        xpRequirements.clear()
+        xpRequirements.addAll(config.getInts("level-xp-requirements"))
+
         maxLevel = xpRequirements.size - 1
+
         rewards.clear()
         for (string in config.getStrings("rewards.rewards")) {
             val split = string.split("::")
