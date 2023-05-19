a11b97bcef05b36f0f3ecae1855695fd2591667b
Auxilor <william.favierparsons1@gmail.com>
2023-05-19 10:47:34 +0100

    Fixed packet bug for some users

--- eco-core/core-nms/v1_19_R1/src/main/kotlin/com/willfp/ecoskills/proxy/v1_19_R1/ActionBarCompatibility.kt
+++ eco-core/core-nms/v1_19_R1/src/main/kotlin/com/willfp/ecoskills/proxy/v1_19_R1/ActionBarCompatibility.kt
@@ -7,12 +7,18 @@ import net.minecraft.network.protocol.game.ClientboundSetActionBarTextPacket
 import net.minecraft.network.protocol.game.ClientboundSystemChatPacket
 
 class ActionBarCompatibility : ActionBarCompatibilityProxy {
+    private val ClientboundSystemChatPacket.isActionBar: Boolean
+        get() = this::class.java.declaredFields
+            .first { it.type == Boolean::class.java }
+            .apply { isAccessible = true }
+            .get(this) as Boolean
+
     override fun onSend(event: PacketEvent) {
         val player = event.player
 
         when (val packet = event.packet.handle) {
             is ClientboundSetActionBarTextPacket -> player.pausePersistentActionBar()
-            is ClientboundSystemChatPacket -> if (packet.c()) player.pausePersistentActionBar()
+            is ClientboundSystemChatPacket -> if (packet.isActionBar) player.pausePersistentActionBar()
         }
     }
 }
--- eco-core/core-nms/v1_19_R3/src/main/kotlin/com/willfp/ecoskills/proxy/v1_19_R3/ActionBarCompatibility.kt
+++ eco-core/core-nms/v1_19_R3/src/main/kotlin/com/willfp/ecoskills/proxy/v1_19_R3/ActionBarCompatibility.kt
@@ -7,12 +7,18 @@ import com.willfp.ecoskills.actionbar.pausePersistentActionBar
 import net.minecraft.network.protocol.game.ClientboundSystemChatPacket
 
 class ActionBarCompatibility : ActionBarCompatibilityProxy {
+    private val ClientboundSystemChatPacket.isActionBar: Boolean
+        get() = this::class.java.declaredFields
+            .first { it.type == Boolean::class.java }
+            .apply { isAccessible = true }
+            .get(this) as Boolean
+
     override fun onSend(event: PacketEvent) {
         val player = event.player
 
         when (val packet = event.packet.handle) {
             is ClientboundSetActionBarTextPacket -> player.pausePersistentActionBar()
-            is ClientboundSystemChatPacket -> if (packet.c()) player.pausePersistentActionBar()
+            is ClientboundSystemChatPacket -> if (packet.isActionBar) player.pausePersistentActionBar()
         }
     }
 }
