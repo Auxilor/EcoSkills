5164e1374f8b26fab83e5923e8e98e5bec131a76
_OfTeN_ <dkoften@gmail.com>
2023-06-02 01:53:11 +0300

    Fixed stupidity

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/skills/display/LevelUpDisplay.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/skills/display/LevelUpDisplay.kt
@@ -27,7 +27,7 @@ class LevelUpDisplay(
         val skill = event.skill
         val level = event.level
 
-        if ((skill.getConfigFor("gain-xp.boss-bar.color")).getBool("gain-xp.boss-bar.color")) {
+        if ((skill.getConfigFor("level-up.message.enabled")).getBool("level-up.message.enabled")) {
             val rawMessage = skill.getConfigFor("level-up.message.message")
                 .getStrings("level-up.message.message")
 
