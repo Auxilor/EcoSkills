0de982f6f64dfdc927917fcb4f02c6ec4766fa67
Kapitowa <antonagashkin1@gmail.com>
2023-08-06 13:51:31 +0300

    GUI optimisation

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/gui/components/StatIcon.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/gui/components/StatIcon.kt
@@ -1,5 +1,6 @@
 package com.willfp.ecoskills.gui.components
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.eco.core.gui.slot
@@ -7,7 +8,15 @@ import com.willfp.eco.core.items.Items
 import com.willfp.eco.core.items.builder.modify
 import com.willfp.eco.util.lineWrap
 import com.willfp.ecoskills.api.getStatLevel
+import com.willfp.ecoskills.plugin
 import com.willfp.ecoskills.stats.Stat
+import org.bukkit.inventory.ItemStack
+import java.util.UUID
+import java.util.concurrent.TimeUnit
+
+private val itemBuilderCache = Caffeine.newBuilder()
+    .expireAfterWrite(plugin.configYml.getInt("gui.refresh-time").toLong(), TimeUnit.SECONDS)
+    .build<MutableMap<UUID, Stat>, ItemStack>()
 
 class StatIcon(
     stat: Stat,
@@ -17,21 +26,23 @@ class StatIcon(
     private val baseIcon = Items.lookup(config.getString("icon")).item
 
     private val slot = slot { player, _ ->
-        val level = player.getStatLevel(stat)
+        itemBuilderCache.get(mutableMapOf(player.uniqueId to stat)) {
+            val level = player.getStatLevel(stat)
 
-        baseIcon.clone().modify {
-            setDisplayName(
-                plugin.configYml.getFormattedString("stats-gui.stat-icon.name")
-                    .replace("%stat%", stat.name)
-                    .let { stat.addPlaceholdersInto(it, level) }
-            )
+            baseIcon.clone().modify {
+                setDisplayName(
+                    plugin.configYml.getFormattedString("stats-gui.stat-icon.name")
+                        .replace("%stat%", stat.name)
+                        .let { stat.addPlaceholdersInto(it, level) }
+                )
 
-            addLoreLines(
-                stat.addPlaceholdersInto(
-                    plugin.configYml.getStrings("stats-gui.stat-icon.lore"),
-                    player
-                ).lineWrap(plugin.configYml.getInt("stats-gui.stat-icon.line-wrap"))
-            )
+                addLoreLines(
+                    stat.addPlaceholdersInto(
+                        plugin.configYml.getStrings("stats-gui.stat-icon.lore"),
+                        player
+                    ).lineWrap(plugin.configYml.getInt("stats-gui.stat-icon.line-wrap"))
+                )
+            }
         }
     }
 
