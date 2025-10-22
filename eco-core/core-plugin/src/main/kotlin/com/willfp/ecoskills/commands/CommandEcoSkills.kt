a5479b1754e23db6a41cf951e69684b897df1b87
joogiebear <135996686+joogiebear@users.noreply.github.com>
2025-10-21 22:51:35 -0500

    Add /ecoskills gain command for XP with action bar notifications
    Adds a new /ecoskills gain command that gives skill XP with visual and audio feedback, complementing the existing silent /ecoskills
      give command.

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/commands/CommandEcoSkills.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/commands/CommandEcoSkills.kt
@@ -13,6 +13,7 @@ class CommandEcoSkills(plugin: EcoPlugin) : PluginCommand(
     init {
         this.addSubcommand(CommandReload(plugin))
             .addSubcommand(CommandGive(plugin))
+            .addSubcommand(CommandGain(plugin))
             .addSubcommand(CommandReset(plugin))
             .addSubcommand(CommandRecount(plugin))
             .addSubcommand(CommandSet(plugin))
