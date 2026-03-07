6f22b7f160c956d90a0674bc8f1bcc59781e6ada
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-03-07 14:34:36 +0000

    Update dependencies to latest versions: eco to 6.77.6, libreforge to 4.81.0, and version to 3.69.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ allprojects {
     }
 
     dependencies {
-        compileOnly("com.willfp:eco:6.77.0")
+        compileOnly("com.willfp:eco:6.77.6")
         compileOnly("org.jetbrains:annotations:23.0.0")
         compileOnly("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
     }
