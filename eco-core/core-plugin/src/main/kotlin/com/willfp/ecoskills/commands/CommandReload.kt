ab7b45c1dddf7cf12b5a1eaa28e4dacfaf74056b
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-03-23 16:22:50 +0000

    Enhance reload command feedback by including loaded skills and stats count in the message

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/commands/CommandReload.kt
@@ -4,6 +4,8 @@ import com.willfp.eco.core.command.impl.Subcommand
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.toNiceString
 import com.willfp.ecoskills.plugin
+import com.willfp.ecoskills.skills.Skills
+import com.willfp.ecoskills.stats.Stats
 import org.bukkit.command.CommandSender
 
 object CommandReload : Subcommand(
@@ -16,6 +18,8 @@ object CommandReload : Subcommand(
         sender.sendMessage(
             plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
                 .replace("%time%", plugin.reloadWithTime().toNiceString())
+                .replace("%stats%", Stats.values().size.toNiceString())
+                .replace("%skills%", Skills.values().size.toNiceString())
         )
     }
 }
