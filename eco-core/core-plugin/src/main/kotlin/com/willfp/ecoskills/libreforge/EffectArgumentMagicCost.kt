bd0424e60db3aa836d334ee5430205774c2958ac
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-06-15 09:26:51 +0100

    fix: update magic cost expression to use player context (#214)

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/libreforge/EffectArgumentMagicCost.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoskills/libreforge/EffectArgumentMagicCost.kt
@@ -6,7 +6,6 @@ import com.willfp.libreforge.ConfigurableElement
 import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.effects.arguments.EffectArgument
 import com.willfp.libreforge.get
-import com.willfp.libreforge.getIntFromExpression
 import com.willfp.libreforge.triggers.DispatchedTrigger
 import org.bukkit.entity.Player
 
@@ -14,7 +13,7 @@ class EffectArgumentMagicCost(private val type: MagicType) : EffectArgument<NoCo
     override fun isMet(element: ConfigurableElement, trigger: DispatchedTrigger, compileData: NoCompileData): Boolean {
         val player = trigger.dispatcher.get<Player>() ?: return false
 
-        val cost = element.config.getIntFromExpression("${type.id}_cost", trigger.data)
+        val cost = element.config.getIntFromExpression("${type.id}_cost", player)
 
         return player.magic[type] >= cost
     }
@@ -22,7 +21,7 @@ class EffectArgumentMagicCost(private val type: MagicType) : EffectArgument<NoCo
     override fun ifMet(element: ConfigurableElement, trigger: DispatchedTrigger, compileData: NoCompileData) {
         val player = trigger.dispatcher.get<Player>() ?: return
 
-        val cost = element.config.getIntFromExpression("${type.id}_cost", trigger.data)
+        val cost = element.config.getIntFromExpression("${type.id}_cost", player)
 
         player.magic[type] -= cost
     }
